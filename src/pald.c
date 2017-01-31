4dae6bdaa0f244751531e75d7df8e13744f49c5c
Gordon Sherwin <gsherwin@extremenetworks.com>
2017-01-31 16:04:02 -0500

    Temp fix for config.xml location on target

--- src/pald.c
+++ src/pald.c
@@ -76,8 +76,8 @@ static const int PAS_MAX_DIR_SIZE = 256;
 static cps_api_operation_handle_t cps_hdl;
 static char *progname, *config_filename, *fuse_mount_dir;
 static bool pas_status, diag_mode;
-/* EXTREME_HACK static char PAS_CONFIG_FILENAME_DFLT[] = "/etc/opx/pas/config.xml"; */
-static char PAS_CONFIG_FILENAME_DFLT[] = "/etc/opx/dell-s6000/pas/config.xml";
+/* EXTREME_HACK address in snapcraft_vm.yaml static char PAS_CONFIG_FILENAME_DFLT[] = "/etc/opx/pas/config.xml"; */
+static char PAS_CONFIG_FILENAME_DFLT[] = "/vendor/dell/s6000/etc/opx/pas/config.xml";
 static char PAS_FUSE_MOUNT_DIR_DFLT[]  = "/mnt/fuse";
 
 
